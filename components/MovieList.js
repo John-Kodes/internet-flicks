b8d0fd3412bca55a6b624021396ead71f3c33421
John-Kodes <jdanielsemine@gmail.com>
2021-07-07 21:42:40 +0400

    movieListItem created

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,22 +1,21 @@
-import Image from "next/image";
+// Components
+import MovieListItem from "./MovieListItem";
 // Styles
 import styles from "@/styles/MovieList.module.scss";
 
 const MovieList = ({ category = "Category" }) => {
+  const moviesArr = [];
+
+  for (let i = 0; i < 6; i++) {
+    moviesArr.push(<MovieListItem key={i} />);
+  }
+
   return (
     <div className={styles.container}>
       <h2>{category}</h2>
       <div className={styles.sliderContainer}>
         <div className={styles.slider}>
-          <div className={styles.sliderMask}>
-            <div className={styles.testThumbnail}>test</div>
-            <div className={styles.testThumbnail}>test</div>
-            <div className={styles.testThumbnail}>test</div>
-            <div className={styles.testThumbnail}>test</div>
-            <div className={styles.testThumbnail}>test</div>
-            <div className={styles.testThumbnail}>test</div>
-            <div className={styles.testThumbnail}>test</div>
-          </div>
+          <div className={styles.sliderMask}>{[...moviesArr]}</div>
         </div>
       </div>
     </div>
---
+++ components/MovieListItem.js
@@ -0,0 +1,8 @@
+// Styles
+import styles from "@/styles/MovieListItem.module.scss";
+
+const MovieListItem = () => {
+  return <div className={styles.container}>item</div>;
+};
+
+export default MovieListItem;
