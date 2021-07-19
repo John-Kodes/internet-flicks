6dd690bbcc7c7b1def750db673dedd07eb8b1019
John-Kodes <jdanielsemine@gmail.com>
2021-07-19 23:38:18 +0400

    login and register pages made

--- pages/account/login.js
+++ pages/account/login.js
@@ -1,7 +1,53 @@
+// React and Next
+import { useState, useEffect, useContext } from "react";
+import Link from "next/dist/client/link";
+// NPM
+import { FaUser } from "react-icons/fa";
+// Styles
+import styles from "@/styles/accountPage.module.scss";
+
 const LoginPage = () => {
   return (
-    <div>
-      <h1>Login</h1>
+    <div className={styles.container}>
+      <div className={styles.card}>
+        <h1 className={styles.title}>
+          <FaUser /> Login
+        </h1>
+        <form className={styles.form}>
+          <div className={styles.inputBox}>
+            <label htmlFor="email" className={styles.label}>
+              EMAIL
+            </label>
+            <input
+              className={styles.field}
+              type="email"
+              name="email"
+              id="email"
+              placeholder="Helene@email.com"
+            />
+          </div>
+          <div className={styles.inputBox}>
+            <label htmlFor="password" className={styles.label}>
+              PASSWORD
+            </label>
+            <input
+              className={styles.field}
+              type="password"
+              name="password"
+              id="password"
+            />
+          </div>
+          <button className={styles.loginBtn} type="submit">
+            LOGIN
+          </button>
+          <div className={styles.register}>
+            Don't have an account?{" "}
+            <Link href="/account/register">
+              <a className={styles.link}>Register</a>
+            </Link>
+          </div>
+        </form>
+      </div>
     </div>
   );
 };
