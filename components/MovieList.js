bf2ff336caae0415d8464c61c8a9b99dde7df2c8
John-Kodes <jdanielsemine@gmail.com>
2021-08-02 21:58:29 +0400

    movielist scroll btn styling done

--- components/MovieList.js
+++ components/MovieList.js
@@ -1,11 +1,17 @@
+import { useState } from "react";
+import Image from "next/dist/client/image";
 // Components
 import MovieListItem from "@/components/MovieListItem";
 // Helpers
 import { defaultMovie } from "@/helpers/index";
+// Images
+import ArrowThinIcon from "@/images/ArrowThinIcon.svg";
 // Styles
 import styles from "@/styles/MovieList.module.scss";
 
 const MovieList = ({ category = "Category", movies = [defaultMovie] }) => {
+  const [isHover, setIsHover] = useState(false);
+
   const movieItems = movies.map((movie, i) => (
     <div className={styles.item}>
       <MovieListItem key={i} movie={movie} />
@@ -15,6 +21,18 @@ const MovieList = ({ category = "Category", movies = [defaultMovie] }) => {
   return (
     <div className={styles.container}>
       <h2>{category}</h2>
+
+      <a className={styles.btnScrollLeft}>
+        <div className={styles.svg}>
+          <Image src={ArrowThinIcon} layout="fill" objectFit="contain" />
+        </div>
+      </a>
+      <a className={styles.btnScrollRight}>
+        {" "}
+        <div className={styles.svg}>
+          <Image src={ArrowThinIcon} layout="fill" objectFit="contain" />
+        </div>
+      </a>
       <div className={styles.sliderContainer}>
         <div className={styles.slider}>
           <div className={styles.sliderMask}>{[...movieItems]}</div>
